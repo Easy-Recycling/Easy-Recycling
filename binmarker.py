7148e067530f54977f641f04c7ea2cec32d4e1b9
Jeff <61351334+mynemjef@users.noreply.github.com>
2021-03-13 14:20:37 +0300

    Add files via upload

--- binmarker.py
+++ binmarker.py
@@ -2,4 +2,4 @@ from kivy.garden.mapview import MapMarkerPopup
 
 
 class BinMarker(MapMarkerPopup):
-    bin_data = []
+    pass
--- main.py
+++ main.py
@@ -1,15 +1,54 @@
-from kivymd.app import MDApp
 from kivy.app import App
-from binsmapview import SelectBin
+from kivymd.app import MDApp
+from kivy.uix.button import Button
+from kivy.garden.mapview import MapView
+from kivy.clock import Clock
 from binmarker import BinMarker
-from binsmapview import BinsMapView
 import sqlite3
 
-#todo - suicide
 
-class MainApp(MDApp):
+class BinsMapView(MapView):
+    getting_bins_timer = None
+    bin_names = []
+
+    def start_getting_bins_in_fov(self):
+        try:
+            self.getting_bins_timer.cancel()
+        except:
+            pass
+
+        self.getting_bins_timer = Clock.schedule_once(self.get_bins_in_fov, 1)
+
+    def get_bins_in_fov(self, *args):
+        min_lat, min_lon, max_lat, max_lon = self.get_bbox()
+        app = App.get_running_app()
+        sql_statement = "SELECT * FROM bins WHERE location_x > %s AND location_x < %s AND location_y > %s AND location_y < %s "%(min_lat, max_lat, min_lon, max_lon)
+        app.cursor.execute(sql_statement)
+        bins = app.cursor.fetchall()
+        print(bins)
+        for bin in bins:
+            name = bin[0]
+            if name in self.bin_names:
+                continue
+            else:
+                self.add_bin(bin)
+
+    def add_bin(self, bin):
+        lat, lon = bin[1], bin[2]
+        marker = BinMarker(lat=lat, lon=lon, source="marker.png")
+        marker.add_widget(Button(size_hint=(1, .4), text=('Select Bin'), on_release=(self.proceed)))
+        self.add_widget(marker)
+        name = bin[0]
+        self.bin_names.append(name)
+
+    def proceed(self, *args):
+        print("proceed")
+
+
+class MainApp(MDApp, MapView):
     latitude = 42.69
     longitude = 23.34
+    selected_name = []
 
     connection = None
     cursor = None
@@ -31,6 +70,7 @@ class MainApp(MDApp):
         self.calculate_closest(bins_sum)
 
     def calculate_closest(self, bins_sum):
+        global closest_bin
         loc_x, loc_y = 42.712630, 23.259306
         min = 0
         location_sum = loc_x + loc_y
@@ -39,22 +79,25 @@ class MainApp(MDApp):
             if distance < 0:
                 distance *= -1
 
-            if i == 1:
+            if i == 0:
                 min = distance
                 closest_bin = i
 
             if distance < min:
                 min = distance
                 closest_bin = i
+
         app = App.get_running_app()
         sql_statement = "SELECT * FROM bins"
         app.cursor.execute(sql_statement)
         bins = app.cursor.fetchall()
-        i = 1
+        i = 0
         for bin in bins:
-            i+=1
             if i == closest_bin:
-                self.SelectBin(bin)
+                self.root.center_on(bin[1], bin[2])
+                self.root.zoom = 17
+
+            i += 1
 
 
     def build(self):
